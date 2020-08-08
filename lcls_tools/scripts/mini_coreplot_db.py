962717d9e06fa24fbf7cbfa0fd54999fc890e57d
Nicole Neveu <nneveu@stanford.edu>
2020-08-07 17:15:18 -0700

    cleaning up cor plot db

--- lcls_tools/scripts/mini_coreplot_db.py
+++ lcls_tools/scripts/mini_coreplot_db.py
@@ -1,6 +1,6 @@
 import glob
 import h5py 
-import numpy 
+import numpy as np 
 import sys
 sys.path.append('/nfs/slac/staas/fs1/g/accelerator_modeling/nneveu/lcls-tools/lcls_tools/cor_plot')
 sys.path.append('../image_processing')
@@ -9,6 +9,7 @@ sys.path.append('../data_analysis')
 from cor_plot_mat_scan import CorPlotMatScan as CPMS
 from mat_image import MatImage as MI
 from archiver import get_iso_time, datenum_to_datetime, save_mat_image_to_h5, save_pvdata_to_h5
+
 
 #Correlation plot scan data
 date  = '/mccfs2/u1/lcls/matlab/data/2020/2020-06/2020-06-21/'
@@ -20,7 +21,11 @@ ctrlpv    = cpms.ctrl_pv
 solvals   = cpms.ctrl_vals
 matstamp  = cpms.timestamp
 beam_data = cpms.beam
+nsamples  = cpms.samples     #n samples per magnet setting
+nsteps    = cpms.iterations  #number of magnet settings
+pvunits   = cpms.control_dict[0]['egu'] #units for sol strength
 
+    
 # Injector PV keys
 pv_names = ['IRIS:LR20:%','%:IN20:%:BACT','ACCL:IN20:300:L0A_PDES','ACCL:IN20:400:L0B_PDES', 'SIOC:SYS0:ML01:CALCOUT008']
 
@@ -33,25 +38,39 @@ cp_group = cp_h5.create_group('YAG02')
 timestamp = datenum_to_datetime(matstamp)
 isotime   = get_iso_time(timestamp)
 cp_group.attrs['isotime'] = isotime
+cp_group.attrs['information'] = 'Solenoid scan last about 5 minutes, the timestamp is associated with the end of the scan.'
 
 # Save pv data given time stamp
 save_pvdata_to_h5(pv_names,cp_group, isotime)
 
 # Save beam data and magnet strengths
-beam = cp_group.create_group('beam_data')
-beam.create_dataset('magnet_strengths', data = solvals)
+beam    = cp_group.create_group('beam_data')
+beam.attrs['information'] = 'Each step is taken at a different solenoid value, the step number matches the index of the solenoid data. There are five samples per step, and several fit methods used to calculate the beam size. Fit methods are used as the name of the samaple set.'
+
+magdata = beam.create_dataset(ctrlpv, data = solvals)
+magdata.attrs['unit'] = pvunits
+magdata.attrs['information'] = 'This is the pv changed during the scan, and resulting values'
 
 fits = ['Gaussian', 'Asymmetric', 'Super', 'RMS', 'RMS cut peak', 'RMS cut area', 'RMS floor']
 
-for i in range(len(solvals)):
+for i in range(0, nsteps):
     step_data  = beam_data[i]
-    step_group = beam.create_group('solval='+str(solvals[i]))
+    step_group = beam.create_group('Step'+str(i))
+
+    xvals = step_group.create_group('beam_sizes_x')
+    yvals = step_group.create_group('beam_sizes_y')
     #import pdb; pdb.set_trace()
 
-    for n in range(len(step_data)):
-        sample = step_group.create_group('sample'+str(n))
-        for j, fit in enumerate(fits):
-            sample.create_dataset(fit+'_xStat', data = step_data[n][j]['xStat'])
-            sample.create_dataset(fit+'_yStat', data = step_data[n][j]['yStat'])
+    for fit in range(0,len(fits)):
+        xdata = []
+        ydata = []
+        for sample in range(0,nsamples):
+            xdata.append(((step_data[sample][fit]['xStat'])[0])[2])
+            ydata.append(((step_data[sample][fit]['yStat'])[0])[2])
 
+        savex = xvals.create_dataset(fits[fit], data=np.array(xdata))
+        savey = yvals.create_dataset(fits[fit], data=np.array(ydata))
+
+        savex.attrs['unit'] = 'um'
+        savey.attrs['unit'] = 'um' 
 cp_h5.close()
