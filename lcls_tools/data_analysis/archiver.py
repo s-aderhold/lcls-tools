875319a230d016e7f2d30f4f60c0edc4d86bccd3
nneveu <nneveu@slac.stanford.edu>
2020-08-31 03:30:57 -0700

    new dataset updates to archiver funcs

--- lcls_tools/data_analysis/archiver.py
+++ lcls_tools/data_analysis/archiver.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 import meme.archive 
 import meme.names 
+from epics import PV
 
 def datenum_to_datetime(datenum):
     """Convert Matlab datenum into Python datetime.
@@ -13,10 +14,6 @@ def datenum_to_datetime(datenum):
     return datetime.fromordinal(int(datenum)) \
            + timedelta(days=days) \
            - timedelta(days=366)
-
-def get_iso_time(pytime):
-    """Return iso time from pyton datetime"""
-    return pytime.isoformat()
 
 def save_mat_image_attributes(mi,h5):
     """Save mat image attrs to h5 dataset or group"""
@@ -36,29 +33,45 @@ def save_mat_image_to_h5(mi, h5group):
        h5group -- h5 group or dataset to save attributes
     """ 
     timestamp   = datenum_to_datetime(mi.timestamp)
-    isotime     = get_iso_time(timestamp)
+    isotime     = timestamp.isoformat()
     image_group = h5group.create_group(isotime)
     image_data  = image_group.create_dataset('image', data=mi.image)
     image_data.attrs['isotime'] = isotime
     save_mat_image_attributes(mi, image_data)
     return image_group
 
-def save_pvdata_to_h5(pv_list, h5group):
-    """Save pvdata at specifc python isotime to h5 file"""
-    timestamp = h5group['image'].attrs['isotime']
-    pv_group  = h5group.create_group('pvdata')
-    pv_names  = []
-    for pv_name in pv_list:
+def get_pvdata_names(partial_listv):
+    """Get list of pv names using wildcards and meme"""
+    #timestamp = h5group['image'].attrs['isotime']
+    #pv_group  = h5group.create_group('pvdata')
+    #pv_names  = []
+    for pv_name in partial_list:
         # Using wildcard to get all related PV names w/ meme
         pv_names  = pv_names + meme.names.list_pvs(pv_name)
    
-    # Converting isotime to datetime for meme 
-    time      = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f')
-    delta     = time + timedelta(days=0, milliseconds=1)
-    pv_values = meme.archive.get(pv_names, from_time=time, to_time=delta)
-    for entry in pv_values:
-        label = entry['pvName']
-        value = entry['value']['value']['values']
-        pv_group.attrs[label] = value 
+    ## Converting isotime to datetime for meme 
+    #time      = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f')
+    #delta     = time + timedelta(days=0, milliseconds=1)
+    #pv_values = meme.archive.get(pv_names, from_time=time, to_time=delta)
+    #for entry in pv_values:
+    #    label = entry['pvName']
+    #    value = entry['value']['value']['values']
+    #    pv_group.attrs[label] = value 
 
-    return None 
+    return pv_list 
+
+def save_pvdata_to_h5(pvlist, h5group, isotime):
+    """Save pv data given timestamp to h5 file"""
+    pv_group = h5group.create_group('pvdata')
+    for pv_name in pvlist:
+   	# get pv unit 
+        pv       = PV(pv_name)
+        d        = pv.get_ctrlvars()
+        units    = d['units']
+	# get pv value
+        pv_value = meme.archive.get(pv_name, from_time=isotime, to_time=isotime)
+        pv_num   = pv_value['values'][0]
+	# save unit and value
+        pv_group.attrs[pv_name] = [str(pv_num), units]
+
+    return None	
