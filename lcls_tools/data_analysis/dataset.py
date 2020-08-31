db032d1413dfbbea7f80dcb0adf2ae00fe3228a7
nneveu <nneveu@slac.stanford.edu>
2020-08-31 02:47:14 -0700

    removing redundant data

--- lcls_tools/data_analysis/dataset.py
+++ lcls_tools/data_analysis/dataset.py
@@ -1,12 +1,23 @@
 import numpy as np
 from cor_plot_mat_scan import CorPlotMatScan as CPMS
+from mat_emit_scan import MatEmitScan as MES
 from archiver import *
+from epics import PV
 
 FITS = ['Gaussian', 'Asymmetric', 'Super', 'RMS', 'RMS cut peak', 'RMS cut area', 'RMS floor']
 #class Database(object):
 #    def __init__(self):
 #        try 
 
+def check_emitscan_load(filename):
+    """Load emit scan data"""
+    mes      = MES(filename)
+    #ctrlpv   = mes.name
+    quad     = mes.quad_name
+    quadvals = mes.quad_vals
+    emitx    = mes.emit_x
+    emity    = mes.emit_y
+    return mes.timestamp
 
 
 def check_corplot_load(filename):
@@ -23,34 +34,53 @@ def check_corplot_load(filename):
 
 
 def unpack_mat_beam_data(beam_data, category):
-    """Gross"""
+    """Gross...
+    For the moment only saving stats. x/yStats are repeats mostly.
+
+    NOTE: 
+    Raw data (X/YPROF) is repeated in the following methods:
+            - Gaussian
+            - Asymmetric
+            - Super
+            - RMS
+    Modified data (X/YPROF, X/YFIT) is in the following methods:
+            - RMS Cut peak
+            - RMS Cut area
+            - RMS Floor 
+
+    X/YCOORD is repeated in ALL metods.
+
+    """
     data ={} 
 
-    profkeys = ['COORD','PROF','FIT'] 
+    #profkeys = ['COORD','PROF','FIT'] 
 
-    if 'profx' in category:
-        data_keys = ['X'+pkey for pkey in profkeys] 
-    elif 'profy' in category:
-        data_keys = ['Y'+pkey for pkey in profkeys] 
-    elif 'profu' in category:
-        data_keys = ['U'+pkey for pkey in profkeys]
-    elif ('xStat' in category) or ('yStat' in category) or ('uStat' in category):
-        data_keys = ['SUM', 'MEAN', 'RMS', 'SKEW', 'KURTOSIS']
-        beam_data = beam_data[0]
-    elif ('xStatStd' in category) or ('yStatStd' in category) or ('uStatStd' in category):
-        data_keys = [category] 
-    elif 'stats' in category:
+    #if 'profx' in category:
+    #    data_keys = 'XFIT' #['X'+pkey for pkey in profkeys] 
+    #elif 'profy' in category:
+    #    data_keys = 'YFIT' #['Y'+pkey for pkey in profkeys] 
+    #elif 'profu' in category:
+    #    data_keys = ['U'+pkey for pkey in profkeys]
+    #elif ('xStat' in category) or ('yStat' in category): #or ('uStat' in category):
+    #    data_keys = ['SUM', 'MEAN', 'RMS', 'SKEW', 'KURTOSIS']
+    #    beam_data = beam_data[0]
+    #elif ('xStatStd' in category) or ('yStatStd' in category): #or ('uStatStd' in category):
+    #    data_keys = [category] 
+    if 'stats' in category:
         data_keys = ['XMEAN', 'YMEAN', 'XRMS', 'YRMS', 'CORR', 'SUM']
         beam_data = beam_data[0]
     elif 'statsStd' in category:
         data_keys = [category]
+    #elif 'method' in category:
+    #    data_keys = [category]
     
-    if 'method' in category:
-        return None
-    else:
+    try:
         for index, dtype in enumerate(data_keys):
             data[dtype] = beam_data[index]
-        return data
+    except:
+        pass #print('Not saving this key:', category)
+
+    return data
 
 def save_corplot_solenoid_scan(filename, h5group):
     """Load corplot data, save to h5"""
@@ -58,13 +88,13 @@ def save_corplot_solenoid_scan(filename, h5group):
     #Make sure only one SOLN ctrl pv:
     assert isinstance(cpms.ctrl_pv, str) 
     assert 'SOLN' in cpms.ctrl_pv
+    assert cpms.beam_names is not None
 
     # Save some default info on top level
     try:
         #import pdb; pdb.set_trace()
         pydatetime = datenum_to_datetime(cpms.timestamp)
         isotime    = pydatetime.isoformat()+'-07:00'
-        
         h5group.attrs['file']         = cpms.file
         h5group.attrs['isotime']      = isotime   
         h5group.attrs['accelerator']  = cpms.accelerator
@@ -74,7 +104,9 @@ def save_corplot_solenoid_scan(filename, h5group):
         h5group.attrs['matlab_timestamp'] = cpms.timestamp
     except:
         print('ERROR: Missing attribute, or unexpected file format')
- 
+        #import pdb; pdb.set_trace()
+
+    print('Trying to load the following types of data:', cpms.beam_names)
     # Loop through measurment steps
     for i in range(0, cpms.iterations):
         step_group = h5group.create_group('Step'+str(i))
@@ -82,34 +114,55 @@ def save_corplot_solenoid_scan(filename, h5group):
         step_group.attrs[cpms.ctrl_pv]        = cpms.ctrl_vals[i]
         step_group.attrs[cpms.ctrl_pv+'.EGU'] = cpms.control_dict[0]['egu'] 
         step_data  = cpms.beam[i]
-        # Create groups to save beam size data
-        print('Trying to load the following types of data:', cpms.beam_names)
-        # Different types of beam data
-        for name in cpms.beam_names:
+        # Looping over samples, i.e. # magnet settings
+        for sample in range(0,cpms.samples):
+            sample_group = step_group.create_group('sample'+str(sample))
+            raw_group    = sample_group.create_group('raw_data')
             # Fitting functions used to calc beam sizes
-            for fit in range(0,len(FITS)):
-                # Looping over samples, i.e. # magnet settings
-                for sample in range(0,cpms.samples):
+            for ifit, fit in enumerate(FITS):
+                if 'Gaussian' in fit:
+                    # 0 index = COORD
+                    # 1 index = PROF
+                    xdata = step_data[sample][ifit]['profx']
+                    ydata = step_data[sample][ifit]['profy']
+                    raw_group.create_dataset('XCOORD', data=xdata[0])
+                    raw_group.create_dataset('XPROF', data=xdata[1])
+                    raw_group.create_dataset('YCOORD', data=ydata[0])
+                    raw_group.create_dataset('YPROF', data=ydata[1])
+              
+                # Create groups to save beam size data
+                fit_group  = sample_group.create_group(fit) 
+                # Different types of beam data
+                for name in cpms.beam_names:
+                   
                     try:
-                        sample_group = step_group.create_group('sample'+str(sample)) 
-                    except:
-                        pass
-
-                    small_data   = unpack_mat_beam_data(step_data[sample][fit][name], name)
-                    try:    
-                        # Unpacking arrays in certain types of data
+                        # Unpacking a stats data
+                        small_data = unpack_mat_beam_data(step_data[sample][ifit][name], name)
                         for key in small_data:
-                            #import pdb; pdb.set_trace()
-                            data_name = FITS[fit].replace(" ", "_") +'_'+name+'_'+key
-                            save_data = step_group['sample'+str(sample)].create_dataset(data_name, data=np.array(small_data[key]))
+                            data_type = name+'_'+key
+                            save_data = fit_group.create_dataset(data_type, data=np.array(small_data[key]))
                     except:
-                        # Redundant method info
-                        pass 
-
-            if 'Stat' in name:
-                save_data.attrs['unit'] = 'um'
+                        #print('No data for this key:', key)
+                        pass
+    
     return
 
 
 
-
+def save_emit_scan(filename, h5group):
+    '''Load emittance scan matlab data'''
+    mes      = MES(filename)
+    assert 'Emittance-scan' in mes.mat_file
+    quad     = mes.quad_name
+    quadvals = mes.quad_vals
+    emitx    = mes.emit_x
+    emity    = mes.emit_y
+    try:
+        for key in emitx:
+            xname = key+'_x'
+            yname = key+'_y'
+            h5group.create_dataset(xname, data=emitx[key])
+            h5group.create_dataset(yname, data=emity[key])
+    except:
+        print('Could not save emittance data')
+    return 
