d5dbf0297db148b03f381d25adcb88bc49e4c551
Nicole Neveu <nneveu@stanford.edu>
2020-08-31 03:36:44 -0700

    sol scan small fix

--- lcls_tools/scripts/solscan_db.py
+++ lcls_tools/scripts/solscan_db.py
@@ -17,12 +17,19 @@ from dataset import *
 
 #Correlation plot scan data
 #date  = '/mccfs2/u1/lcls/matlab/data/2020/2020-06/2020-06-21/'
-#date  = '/u1/lcls/matlab/data/2020/2020-06/2020-06-21/'
+#solfiles  = glob.glob('/mccfs2/u1/lcls/matlab/data/2020/2020-0{6,7}/2020-0{6,7}-{21,22,8,9}/Cor*SOLN*')
+#import pdb; pdb.set_trace()
 #date      = '/Users/nneveu/Google Drive File Stream/Shared drives/Injector Modeling/measurements/cu_inj/2020-06-21/'
 #dtype     = 'CorrelationPlot-SOLN_IN20_121_BCTRL-2020-06-21-091733.mat'
 #scan_file = date+dtype
 
-solfiles = glob.glob('/Users/nneveu/Google Drive File Stream/Shared drives/Injector Modeling/measurements/cu_inj/2020-0*/Cor*SOLN*')
+#solfiles = glob.glob('/Users/nneveu/Google Drive File Stream/Shared drives/Injector Modeling/measurements/cu_inj/2020-0*/Cor*SOLN*')
+
+sol6 = glob.glob('/mccfs2/u1/lcls/matlab/data/2020/2020-06/2020-06-*[21,22]/Cor*SOLN*')
+sol7 = glob.glob('/mccfs2/u1/lcls/matlab/data/2020/2020-07/2020-07-*[08,09]/Cor*SOLN*')
+
+solfiles = sol6+sol7
+
 print('Number of SOL scans', len(solfiles))
 # Make h5 file, give data description
 cp_h5 = h5py.File('2020_sol_data.h5', 'w')
@@ -44,16 +51,16 @@ for filename in solfiles[:2]:
         beam    = cp_group.create_group('beam_data')
         #import pdb; pdb.set_trace()
         save_corplot_solenoid_scan(filename, beam)
-        
+       
         # Injector PV keys
-        partial_pv = ['IRIS:LR20:130:%','%:IN20:%:BACT','ACCL:IN20:300:L0A_PDES','ACCL:IN20:400:L0B_PDES', 'SIOC:SYS0:ML01:CALCOUT008']
+        partial_pv = ['IRIS:LR20:130:CONFG_SEL','%:IN20:%:BACT','ACCL:IN20:300:L0A_PDES','ACCL:IN20:400:L0B_PDES', 'SIOC:SYS0:ML01:CALCOUT008']
         pv_list = get_pvdata_names(partial_pv)
         
         # Save pv data given time stamp
         pv_group = cp_group.create_group('pvdata')
-        save_pvdata_to_h5(pv_list, pv_group, isotim)
+        save_pvdata_to_h5(pv_list, pv_group, isotime)
 
-   except: 
+    except: 
         pass
 
 cp_h5.close()
